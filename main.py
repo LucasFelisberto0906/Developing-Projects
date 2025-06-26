4b24af031e5d6ef6e499aea121ba041f687061b8
Lucas Felisberto <96011281+LucasFelisberto0906@users.noreply.github.com>
2025-06-27 00:34:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,183 +6,181 @@ pygame.init()
 
 WIDTH = 400
 HEIGHT = 600
-BORDA = 20
+BORDER = 20
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Calculadora")
+pygame.display.set_caption("Calculator")
 
 running = True
 clock = pygame.time.Clock()
 
-# CORES 
-CORPO = (46, 53, 64)
-TELA = (193, 204, 212)
-BOTOES_NUM = (125, 138, 150)
-BOTAO_PONTO = (89, 96, 106)
-BOTAO_OPERACOES = (244, 186, 59)
-BOTAO_IGUAL = (242, 90, 60)
-BOTAO_TEXTO = (255, 255, 255)
+# COLORS
+BODY = (46, 53, 64)
+SCREEN_COLOR = (193, 204, 212)
+NUM_BUTTONS = (125, 138, 150)
+BUTTON_CLEAR_DEL = (89, 96, 106)
+BUTTON_OPERATIONS = (244, 186, 59)
+BUTTON_EQUAL = (242, 90, 60)
+BUTTON_TEXT_COLOR = (255, 255, 255)
 
+# Screen text
+screen_text = ""
 
-# tela
-texto_aparecer_tela = ""
+screen_rect = pygame.Rect(BORDER//2, 60, WIDTH - BORDER, 110)
 
-rect = pygame.Rect(BORDA//2, 60, WIDTH - BORDA, 110)
-
-botoes = [
-	['DEL','C','(',')'],
-	['√','^','e','π'],
-	['7', '8','9','/'],
-	['4','5','6','x'],
-	['1','2','3','-'],
-	['0','.','=','+']
+buttons = [
+    ['DEL','C','(',')'],
+    ['√','^','e','π'],
+    ['7', '8','9','/'],
+    ['4','5','6','x'],
+    ['1','2','3','-'],
+    ['0','.','=','+']
 ]
-
 
 w = WIDTH // 4
 h = 70
-borda_topo = 185
-font_hist = pygame.font.SysFont('comicsans', 20)
+top_border = 185
+font_history = pygame.font.SysFont('comicsans', 20)
 font = pygame.font.SysFont('comicsans', 35)
-fonte_tela = pygame.font.SysFont('comicsans', 60)  # Fonte só para a tela
+screen_font = pygame.font.SysFont('comicsans', 60)  # Font only for the screen
 
-resultado_mostrado = False
-botoes_rects = []
+result_shown = False
+buttons_rects = []
 
-def desenhar_botoes():
-	botoes_rects.clear()
-	
-	for linha in range(6):
-		for coluna in range(4):
-			x = coluna * w + BORDA // 2
-			y = linha * h + borda_topo
-			rotulo = botoes[linha][coluna]
+def draw_buttons():
+    buttons_rects.clear()
 
-			if rotulo in {'+', '-', '/', 'x'}:
-				cor = BOTAO_OPERACOES
-			elif rotulo == '=':
-				cor = BOTAO_IGUAL
-			elif rotulo == 'C' or rotulo == 'DEL':
-				cor = BOTAO_PONTO
-			else:
-				cor = BOTOES_NUM
-			
-			btn_rect = pygame.Rect(x, y, w - BORDA, h - BORDA)
-			pygame.draw.rect(win, cor, btn_rect, border_radius=20)
+    for row in range(6):
+        for col in range(4):
+            x = col * w + BORDER // 2
+            y = row * h + top_border
+            label = buttons[row][col]
 
-			texto = font.render(rotulo, True, BOTAO_TEXTO)
-			texto_rect = texto.get_rect(center=btn_rect.center)
-			win.blit(texto, texto_rect)
+            if label in {'+', '-', '/', 'x'}:
+                color = BUTTON_OPERATIONS
+            elif label == '=':
+                color = BUTTON_EQUAL
+            elif label == 'C' or label == 'DEL':
+                color = BUTTON_CLEAR_DEL
+            else:
+                color = NUM_BUTTONS
+            
+            btn_rect = pygame.Rect(x, y, w - BORDER, h - BORDER)
+            pygame.draw.rect(win, color, btn_rect, border_radius=20)
 
-			botoes_rects.append((btn_rect, rotulo))
+            text_surface = font.render(label, True, BUTTON_TEXT_COLOR)
+            text_rect = text_surface.get_rect(center=btn_rect.center)
+            win.blit(text_surface, text_rect)
 
-	
-def calcular_com_funcoes(expressao: str) -> str:
-	try:
-		expr = expressao
-		expr = expr.replace('x', '*')
-		expr = expr.replace('^', '**')
-		expr = expr.replace('π', 'math.pi')
-		expr = re.sub(r'\be\b', 'math.e', expr)
-		expr = re.sub(r'√\s*\(?([0-9.]+)\)?', r'math.sqrt(\1)', expr)
-		resultado = eval(expr, {"math": math})
+            buttons_rects.append((btn_rect, label))
 
-		if isinstance(resultado, float) and resultado.is_integer():
-			return str(int(resultado))
-		else:
-			return str(round(resultado, 5))
-	except:
-		return "Erro"
+def calculate_expression(expr_str: str) -> str:
+    try:
+        expr = expr_str
+        expr = expr.replace('x', '*')
+        expr = expr.replace('^', '**')
+        expr = expr.replace('π', 'math.pi')
+        expr = re.sub(r'\be\b', 'math.e', expr)
+        expr = re.sub(r'√\s*\(?([0-9.]+)\)?', r'math.sqrt(\1)', expr)
+        result = eval(expr, {"math": math})
 
-# historico
-historico = []
-historico_btn = []
-mostrar_historico = False
-def des_historico(x, y, width, height):
-	his_rect = pygame.Rect(x,y, width, height)
-	pygame.draw.rect(win, (255, 0, 0), his_rect, border_radius=20)
-	text = font_hist.render("HISTÓRICO", True, (255, 255, 255))
-	text_rect = text.get_rect(centery=rect.centery, left=rect.left + 120, top=rect.top - 50)
-	win.blit(text, text_rect)
-	historico_btn.append(his_rect)
+        if isinstance(result, float) and result.is_integer():
+            return str(int(result))
+        else:
+            return str(round(result, 5))
+    except:
+        return "Error"
+
+# History
+history = []
+history_buttons = []
+show_history = False
+
+def draw_history_button(x, y, width, height):
+    his_rect = pygame.Rect(x, y, width, height)
+    pygame.draw.rect(win, (255, 0, 0), his_rect, border_radius=20)
+    text = font_history.render("HISTORY", True, (255, 255, 255))
+    text_rect = text.get_rect(centery=screen_rect.centery, left=screen_rect.left + 120, top=screen_rect.top - 50)
+    win.blit(text, text_rect)
+    history_buttons.append(his_rect)
 
 while running:
-	clock.tick(60)
+    clock.tick(60)
 
-	if mostrar_historico:
-		win.fill(CORPO)
+    if show_history:
+        win.fill(BODY)
 
-		titulo = font.render("HISTÓRICO", True, (255, 255, 255))
-		win.blit(titulo, (WIDTH//2 - titulo.get_width()//2, 30))
+        title = font.render("HISTORY", True, (255, 255, 255))
+        win.blit(title, (WIDTH//2 - title.get_width()//2, 30))
 
-		y_atual = 100
+        current_y = 100
 
-		for i, entrada in enumerate(reversed(historico[-10:])):
-			linha = font.render(entrada, True, (255, 255, 255))
-			win.blit(linha, (40, y_atual))
-			y_atual += 30
+        for i, entry in enumerate(reversed(history[-10:])):
+            line = font.render(entry, True, (255, 255, 255))
+            win.blit(line, (40, current_y))
+            current_y += 30
 
-		voltar_texto = font_hist.render("Clique para voltar", True, (200, 200, 200))
-		win.blit(voltar_texto, (WIDTH//2 - voltar_texto.get_width()//2, HEIGHT - 40),)
+        back_text = font_history.render("Click to go back", True, (200, 200, 200))
+        win.blit(back_text, (WIDTH//2 - back_text.get_width()//2, HEIGHT - 40))
 
-		pygame.display.update()
+        pygame.display.update()
 
-		espera = True
-		while espera:
-			for ev in pygame.event.get():
-				if ev.type == pygame.QUIT:
-					running = False
-					espera = False
-				elif ev.type == pygame.MOUSEBUTTONDOWN:
-					mostrar_historico = False
-					espera = False
-		continue
+        waiting = True
+        while waiting:
+            for ev in pygame.event.get():
+                if ev.type == pygame.QUIT:
+                    running = False
+                    waiting = False
+                elif ev.type == pygame.MOUSEBUTTONDOWN:
+                    show_history = False
+                    waiting = False
+        continue
 
+    win.fill(BODY)
+    pygame.draw.rect(win, SCREEN_COLOR, screen_rect, border_radius=20)
 
-	win.fill(CORPO)
-	pygame.draw.rect(win, TELA, rect, border_radius=20)
+    screen_text_surface = screen_font.render(screen_text, True, (0,0,0))
+    screen_text_rect = screen_text_surface.get_rect(right=screen_rect.right - 20, centery=screen_rect.centery)
+    win.blit(screen_text_surface, screen_text_rect)
 
-	texto_entrada = fonte_tela.render(texto_aparecer_tela, True, (0,0,0))
-	texto_entrada_rect = texto_entrada.get_rect(right=rect.right - 20, centery=rect.centery)
-	win.blit(texto_entrada, texto_entrada_rect)
+    draw_history_button(BORDER//2, BORDER//2, WIDTH - BORDER, 30)
+    draw_buttons()
 
-	des_historico(BORDA//2, BORDA//2, WIDTH - BORDA, 30)
-	desenhar_botoes()
-	
-	pygame.display.update()
+    pygame.display.update()
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-		elif event.type == pygame.MOUSEBUTTONDOWN:
-			for btn_rect, rotulo in botoes_rects:
-				if btn_rect.collidepoint(event.pos):
-					if rotulo == 'C':
-						texto_aparecer_tela = ''
-					elif rotulo == '=':
-						resultado_mostrado = True
-						try:
-							historico.append(texto_aparecer_tela)
-							texto_aparecer_tela = calcular_com_funcoes(texto_aparecer_tela)
-							historico.append(texto_aparecer_tela)
-							for cal in historico:
-								print(cal)
-						except:
-							texto_aparecer_tela = "Erro"
-					elif rotulo == 'DEL':
-						if not resultado_mostrado:
-							texto_aparecer_tela = texto_aparecer_tela[:-1]
-					else:
-						if resultado_mostrado:
-							if rotulo in {'+', '-', '/', 'x', '^'}:
-								texto_aparecer_tela += rotulo
-								resultado_mostrado = False
-							else:
-								texto_aparecer_tela = rotulo
-								resultado_mostrado = False
-						else:
-							texto_aparecer_tela += rotulo
-			for btn_his in historico_btn:
-				if btn_his.collidepoint(event.pos):
-					mostrar_historico = True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            for btn_rect, label in buttons_rects:
+                if btn_rect.collidepoint(event.pos):
+                    if label == 'C':
+                        screen_text = ''
+                    elif label == '=':
+                        result_shown = True
+                        try:
+                            history.append(screen_text)
+                            screen_text = calculate_expression(screen_text)
+                            history.append(screen_text)
+                            for calc in history:
+                                print(calc)
+                        except:
+                            screen_text = "Error"
+                    elif label == 'DEL':
+                        if not result_shown:
+                            screen_text = screen_text[:-1]
+                    else:
+                        if result_shown:
+                            if label in {'+', '-', '/', 'x', '^'}:
+                                screen_text += label
+                                result_shown = False
+                            else:
+                                screen_text = label
+                                result_shown = False
+                        else:
+                            screen_text += label
+            for btn_his in history_buttons:
+                if btn_his.collidepoint(event.pos):
+                    show_history = True
+
 pygame.quit()
